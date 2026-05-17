035ece8e6412ac8b696d1c601fc3f6ea72b38128
Hoang Vo <vohoang2204@gmail.com>
2026-05-17 14:33:54 +0700

    init

--- utils/metrics.py
+++ utils/metrics.py
@@ -17,18 +17,6 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 
-def _unwrap_model(model):
-    return model.module if hasattr(model, "module") else model
-
-
-def _prototype_branch(model):
-    model = _unwrap_model(model)
-    branch = getattr(model, "prototype_branch", None)
-    if branch is None or not branch.is_ready():
-        return None
-    return branch
-
-
 def rank(similarity, q_pids, g_pids, max_rank=10, get_mAP=True):
     if get_mAP:
         indices = torch.argsort(similarity, dim=1, descending=True)
@@ -68,13 +56,6 @@ def get_metrics(similarity, qids, gids, n_, retur_indices=False):
         return [n_, t2i_cmc[0], t2i_cmc[4], t2i_cmc[9], t2i_mAP, t2i_mINP, t2i_cmc[0]+ t2i_cmc[4]+ t2i_cmc[9]], indices
     else:
         return [n_, t2i_cmc[0], t2i_cmc[4], t2i_cmc[9], t2i_mAP, t2i_mINP, t2i_cmc[0]+ t2i_cmc[4]+ t2i_cmc[9]]
-
-
-def _prototype_score_weights(args):
-    weights = getattr(args, "prototype_score_weights", None)
-    if weights is None:
-        weights = [getattr(args, "prototype_score_weight", 0.1)]
-    return [float(weight) for weight in weights]
 
 
 class Evaluator():
@@ -139,24 +120,15 @@ class Evaluator():
 
     def eval(self, model, i2t_metric=False, return_metrics=False):
         qfeats, gfeats, qids, gids = self._compute_embedding(model)
-        qfeats_raw, gfeats_raw = qfeats, gfeats
         qfeats = F.normalize(qfeats, p=2, dim=1) # text features
         gfeats = F.normalize(gfeats, p=2, dim=1) # image features
         sims_global = qfeats @ gfeats.t()
 
-        proto_sims = None
-        branch = _prototype_branch(model)
         if not self.args.only_global:
             vq_feats, vg_feats, _, _ = self._compute_embedding_grab(model)
-            vq_feats_raw, vg_feats_raw = vq_feats, vg_feats
             vq_feats = F.normalize(vq_feats, p=2, dim=1) # text features
             vg_feats = F.normalize(vg_feats, p=2, dim=1) # image features
             sims_grab = vq_feats@vg_feats.t()
-            if branch is not None and branch.use_local:
-                proto_sims = branch.score(vq_feats_raw, vg_feats_raw).cpu()
-
-        if branch is not None and proto_sims is None:
-            proto_sims = branch.score(qfeats_raw, gfeats_raw).cpu()
 
         if self.args.only_global:
             sims_dict = {
@@ -178,16 +150,6 @@ class Evaluator():
                 'global+grab(0.68)': 0.68 * sims_global + 0.32 * sims_grab, # alpha = 0.68
                 'global+grab(0.32)': 0.32 * sims_global + 0.68 * sims_grab # alpha = 0.32
             }
-
-        if proto_sims is not None:
-            proto_rows = {}
-            proto_weights = _prototype_score_weights(self.args)
-            single_default_weight = len(proto_weights) == 1 and getattr(self.args, "prototype_score_weights", None) is None
-            for weight in proto_weights:
-                for key, sims in sims_dict.items():
-                    proto_key = f'{key}+proto' if single_default_weight else f'{key}+proto({weight:g})'
-                    proto_rows[proto_key] = (1.0 - weight) * sims + weight * proto_sims
-            sims_dict.update(proto_rows)
 
         table = PrettyTable(["task", "R1", "R5", "R10", "mAP", "mINP","rSum"])
 
--- utils/options.py
+++ utils/options.py
@@ -64,10 +64,6 @@ def get_args():
     parser.add_argument("--prototype_tau", type=float, default=0.05)
     parser.add_argument("--prototype_hard_k", type=int, default=16)
     parser.add_argument("--prototype_id_weight", type=float, default=0.2)
-    parser.add_argument("--prototype_score_weight", type=float, default=0.1)
-    parser.add_argument("--prototype_score_weights", type=float, nargs='+',
-                        default=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-                        help="Inference-time prototype score weights for interpolation ablation.")
     parser.add_argument("--prototype_momentum", type=float, default=0.2)
     ######################## vison trainsformer settings ########################
     parser.add_argument("--img_size", type=tuple, default=(384, 128))
